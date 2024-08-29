d3b8192c770d97847a5eaad86a6ed96796c197f8
Stian Rusvik <stian.rusvik@no.experis.com>
2024-08-29 14:34:19 +0200

    implemented GET for create language.

---
+++ src/main/java/com/booleanuk/api/api/Languages.java
@@ -0,0 +1,25 @@
+package com.booleanuk.api.api;
+
+import com.booleanuk.api.requests.Language;
+import com.booleanuk.api.requests.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("languages")
+public class Languages {
+    private final List<Language> languages = new ArrayList<>(){{
+        add(new Language("Java"));
+        add(new Language("C#"));
+    }};
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Language create(@RequestBody Language language) {
+        this.languages.add(language);
+        return language;
+    }
+}
--- src/main/java/com/booleanuk/api/requests/Language.java
+++ src/main/java/com/booleanuk/api/requests/Language.java
@@ -7,6 +7,8 @@ public class Language {
         this.name = name;
     }
 
+    public Language() {}
+
     public String getName() {
         return name;
     }
